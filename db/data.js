c58720da5e40cc44aac8865b3d1a78ae2c69366a
santospasman <spasman@udesa.edu.ar>
2023-09-11 09:51:05 -0300

    base de data

---
+++ db/data.js
@@ -0,0 +1,113 @@
+const data = {
+    usuarios: [{
+        id: 1 ,
+        email: "goleroexperto@gmail.com",
+        fotoPerfil: null,
+        clave: "golero123" ,
+        fecNac: "1998-09-24",
+        dni: 98765432
+    },
+    {
+        id: 2,
+        email: "futbolamante@gmail.com",
+        fotoPerfil: null,
+        clave: "amantefulbo",
+        fecNac: "2004-03-05",
+        dni: 36765432
+    },
+    {
+        id: 3,
+        email: "jugadorestrella@gmail.com",
+        fotoPerfil: null,
+        clave: "star",
+        fecNac: "1987-07-31",
+        dni: 47955432
+    },
+    {
+        id: 4,
+        email: "campodejuego@gmail.com",
+        fotoPerfil: null,
+        clave: "campo",
+        fecNac: "2012-01-06",
+        dni: 47928032
+    },
+    {
+        id: 5,
+        email: "entrenadorhabil@gmail.com",
+        fotoPerfil: null,
+        clave: "dt",
+        fecNac: "1987-06-24",
+        dni: 55674032
+    }
+],
+    posteos: [{
+        id: 1,
+        usuarioId: 1,
+        url: "https://i.pinimg.com/736x/49/19/a8/4919a8dc44184ea27159ad8e1e6dde41.jpg" ,
+        pieImg: "Parado no bailão",
+        comentarios: [{usuario: 2,comentario:"Vamos Santos !!!!!"},{usuario: 3,comentario:"Te amo Neymar !!!!!"},{usuario: 4,comentario:"La copa libertadores es mi obsesion"},{usuario: 5,comentario:"Que Jogadorrrr..."}]
+    },
+    {
+        id: 2,
+        usuarioId: 1,
+        url: "https://i.pinimg.com/originals/30/56/6b/30566b73b45265855eec978630163a9e.jpg" ,
+        pieImg: "El ultimo Diez",
+        comentarios: [{usuario: 2,comentario:"EL ULTIMO 10"},{usuario: 3,comentario:"Roman, Boca es tu casa"},{usuario: 4,comentario:"QUIERO LA LIBERTADOOOREEEEE"},{usuario: 5,comentario:"Romanticoooo"}]
+    },
+    {
+        id: 3,
+        usuarioId: 2,
+        url: "https://i.pinimg.com/originals/30/56/6b/30566b73b45265855eec978630163a9e.jpg" ,
+        pieImg: "El ultimo Diez",
+        comentarios: [{usuario: 2,comentario:"EL ULTIMO 10"},{usuario: 3,comentario:"Roman, Boca es tu casa"},{usuario: 4,comentario:"QUIERO LA LIBERTADOOOREEEEE"},{usuario: 5,comentario:"Romanticoooo"}]
+    },
+    {
+        id: 4,
+        usuarioId: 2,
+        url: "https://i.pinimg.com/736x/49/19/a8/4919a8dc44184ea27159ad8e1e6dde41.jpg" ,
+        pieImg: "Parado no bailão",
+        comentarios: [{usuario: 2,comentario:"Vamos Santos !!!!!"},{usuario: 3,comentario:"Te amo Neymar !!!!!"},{usuario: 4,comentario:"La copa libertadores es mi obsesion"},{usuario: 5,comentario:"Que Jogadorrrr..."}]
+    },
+    {
+        id: 5,
+        usuarioId: 3,
+        url: "https://i.pinimg.com/736x/49/19/a8/4919a8dc44184ea27159ad8e1e6dde41.jpg" ,
+        pieImg: "Parado no bailão",
+        comentarios: [{usuario: 2,comentario:"Vamos Santos !!!!!"},{usuario: 3,comentario:"Te amo Neymar !!!!!"},{usuario: 4,comentario:"La copa libertadores es mi obsesion"},{usuario: 5,comentario:"Que Jogadorrrr..."}]
+    },
+    {
+        id: 6,
+        usuarioId: 3,
+        url: "https://i.pinimg.com/736x/49/19/a8/4919a8dc44184ea27159ad8e1e6dde41.jpg" ,
+        pieImg: "Parado no bailão",
+        comentarios: [{usuario: 2,comentario:"Vamos Santos !!!!!"},{usuario: 3,comentario:"Te amo Neymar !!!!!"},{usuario: 4,comentario:"La copa libertadores es mi obsesion"},{usuario: 5,comentario:"Que Jogadorrrr..."}]
+    },
+    {
+        id: 7,
+        usuarioId: 4,
+        url: "https://i.pinimg.com/736x/49/19/a8/4919a8dc44184ea27159ad8e1e6dde41.jpg" ,
+        pieImg: "Parado no bailão",
+        comentarios: [{usuario: 2,comentario:"Vamos Santos !!!!!"},{usuario: 3,comentario:"Te amo Neymar !!!!!"},{usuario: 4,comentario:"La copa libertadores es mi obsesion"},{usuario: 5,comentario:"Que Jogadorrrr..."}]
+    },
+    {
+        id: 8,
+        usuarioId: 4,
+        url: "https://i.pinimg.com/736x/49/19/a8/4919a8dc44184ea27159ad8e1e6dde41.jpg" ,
+        pieImg: "Parado no bailão",
+        comentarios: [{usuario: 2,comentario:"Vamos Santos !!!!!"},{usuario: 3,comentario:"Te amo Neymar !!!!!"},{usuario: 4,comentario:"La copa libertadores es mi obsesion"},{usuario: 5,comentario:"Que Jogadorrrr..."}]
+    },
+    {
+        id: 9,
+        usuarioId: 5,
+        url: "https://i.pinimg.com/736x/49/19/a8/4919a8dc44184ea27159ad8e1e6dde41.jpg" ,
+        pieImg: "Parado no bailão",
+        comentarios: [{usuario: 2,comentario:"Vamos Santos !!!!!"},{usuario: 3,comentario:"Te amo Neymar !!!!!"},{usuario: 4,comentario:"La copa libertadores es mi obsesion"},{usuario: 5,comentario:"Que Jogadorrrr..."}]
+    },
+    {
+        id: 10,
+        usuarioId: 5,
+        url: "https://i.pinimg.com/736x/49/19/a8/4919a8dc44184ea27159ad8e1e6dde41.jpg" ,
+        pieImg: "Parado no bailão",
+        comentarios: [{usuario: 2,comentario:"Vamos Santos !!!!!"},{usuario: 3,comentario:"Te amo Neymar !!!!!"},{usuario: 4,comentario:"La copa libertadores es mi obsesion"},{usuario: 5,comentario:"Que Jogadorrrr..."}]
+    },]
+};
