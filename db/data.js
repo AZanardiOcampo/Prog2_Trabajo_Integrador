80fc07440e7544830d4052e6ed1bd4ca8afcc24f
AZanardiOcampo <azanardiocampo@udesa.edu.ar>
2023-09-11 10:05:44 -0300

    DB pasada a js

--- db/data.js
+++ db/data.js
@@ -45,69 +45,125 @@ const data = {
         usuarioId: 1,
         url: "https://i.pinimg.com/736x/49/19/a8/4919a8dc44184ea27159ad8e1e6dde41.jpg" ,
         pieImg: "Parado no bailão",
-        comentarios: [{usuario: 2,comentario:"Vamos Santos !!!!!"},{usuario: 3,comentario:"Te amo Neymar !!!!!"},{usuario: 4,comentario:"La copa libertadores es mi obsesion"},{usuario: 5,comentario:"Que Jogadorrrr..."}]
+        comentarios: [
+            {usuario: 2,comentario:"Vamos Santos !!!!!"},
+            {usuario: 3,comentario:"Te amo Neymar !!!!!"},
+            {usuario: 4,comentario:"La copa libertadores es mi obsesion"},
+            {usuario: 5,comentario:"Que Jogadorrrr..."}]
     },
     {
         id: 2,
         usuarioId: 1,
         url: "https://i.pinimg.com/originals/30/56/6b/30566b73b45265855eec978630163a9e.jpg" ,
         pieImg: "El ultimo Diez",
-        comentarios: [{usuario: 2,comentario:"EL ULTIMO 10"},{usuario: 3,comentario:"Roman, Boca es tu casa"},{usuario: 4,comentario:"QUIERO LA LIBERTADOOOREEEEE"},{usuario: 5,comentario:"Romanticoooo"}]
+        comentarios: [
+            {usuario: 2,comentario:"EL ULTIMO 10"},
+            {usuario: 3,comentario:"Roman, Boca es tu casa"},
+            {usuario: 4,comentario:"QUIERO LA LIBERTADOOOREEEEE"},
+            {usuario: 5,comentario:"Romanticoooo"}]
     },
     {
         id: 3,
         usuarioId: 2,
-        url: "https://i.pinimg.com/originals/30/56/6b/30566b73b45265855eec978630163a9e.jpg" ,
-        pieImg: "El ultimo Diez",
-        comentarios: [{usuario: 2,comentario:"EL ULTIMO 10"},{usuario: 3,comentario:"Roman, Boca es tu casa"},{usuario: 4,comentario:"QUIERO LA LIBERTADOOOREEEEE"},{usuario: 5,comentario:"Romanticoooo"}]
-    },
+        url: "https://i.pinimg.com/originals/22/ad/21/22ad219ca0f55b0b46c8d79a556a56ae.jpg",
+        pieImg: "SIUUUUUUUUUUU",
+        comentarios: [
+            {usuario: 1, comentario: "THE GOAT"},
+            {usuario: 3, comentario: "Mr. Championsss"},
+            {usuario: 4, comentario: "EL COMANDANTEEEE"},
+            {usuario: 5, comentario: "Messi > Ronaldo"}
+        ]
+    }
+    ,
     {
         id: 4,
         usuarioId: 2,
-        url: "https://i.pinimg.com/736x/49/19/a8/4919a8dc44184ea27159ad8e1e6dde41.jpg" ,
-        pieImg: "Parado no bailão",
-        comentarios: [{usuario: 2,comentario:"Vamos Santos !!!!!"},{usuario: 3,comentario:"Te amo Neymar !!!!!"},{usuario: 4,comentario:"La copa libertadores es mi obsesion"},{usuario: 5,comentario:"Que Jogadorrrr..."}]
-    },
+        url: "https://i.pinimg.com/originals/34/21/2e/34212e4bf0581d9f528907987ff40f58.jpg",
+        pieImg: "Caipirinha, samba e futebol",
+        comentarios: [
+            {usuario: 1, comentario: "Zaaambaaaaa"},
+            {usuario: 3, comentario: "The magician"},
+            {usuario: 4, comentario: "Futeboolll"},
+            {usuario: 5, comentario: "Baila Dinho Baila"}
+        ]
+    }
+    ,
     {
         id: 5,
         usuarioId: 3,
-        url: "https://i.pinimg.com/736x/49/19/a8/4919a8dc44184ea27159ad8e1e6dde41.jpg" ,
-        pieImg: "Parado no bailão",
-        comentarios: [{usuario: 2,comentario:"Vamos Santos !!!!!"},{usuario: 3,comentario:"Te amo Neymar !!!!!"},{usuario: 4,comentario:"La copa libertadores es mi obsesion"},{usuario: 5,comentario:"Que Jogadorrrr..."}]
-    },
+        url: "https://i.pinimg.com/originals/e5/33/20/e533200b8ffded0453a22c0cde450585.jpg",
+        pieImg: "TE AMO ARGENTINAAAA <3 <3",
+        comentarios: [
+            {usuario: 1, comentario: "THE GOAT"},
+            {usuario: 2, comentario: "Muchaaaachooosssss"},
+            {usuario: 4, comentario: "Ronaldo > Messi"},
+            {usuario: 5, comentario: "HERMOSAAAAA"}
+        ]
+    }
+    ,
     {
         id: 6,
         usuarioId: 3,
-        url: "https://i.pinimg.com/736x/49/19/a8/4919a8dc44184ea27159ad8e1e6dde41.jpg" ,
-        pieImg: "Parado no bailão",
-        comentarios: [{usuario: 2,comentario:"Vamos Santos !!!!!"},{usuario: 3,comentario:"Te amo Neymar !!!!!"},{usuario: 4,comentario:"La copa libertadores es mi obsesion"},{usuario: 5,comentario:"Que Jogadorrrr..."}]
-    },
+        url: "https://i.pinimg.com/originals/f4/b3/6c/f4b36cef73d4dbbaf1a765cc97ed2b00.jpg",
+        pieImg: "Kikando e me olhandooo",
+        comentarios: [
+            {usuario: 1, comentario: "Visca Barca"},
+            {usuario: 2, comentario: "Volveeeeee"},
+            {usuario: 4, comentario: "Neymar Barcelona > Neymar PSG"},
+            {usuario: 5, comentario: "Campeonesssss"}
+        ]
+    }
+    ,
     {
         id: 7,
         usuarioId: 4,
-        url: "https://i.pinimg.com/736x/49/19/a8/4919a8dc44184ea27159ad8e1e6dde41.jpg" ,
-        pieImg: "Parado no bailão",
-        comentarios: [{usuario: 2,comentario:"Vamos Santos !!!!!"},{usuario: 3,comentario:"Te amo Neymar !!!!!"},{usuario: 4,comentario:"La copa libertadores es mi obsesion"},{usuario: 5,comentario:"Que Jogadorrrr..."}]
-    },
+        url: "https://i.pinimg.com/originals/ad/f1/a6/adf1a69d625058b82066e88c76665b41.jpg",
+        pieImg: "Vinotinto de mi vida...",
+        comentarios: [
+            {usuario: 1, comentario: "Grande Josefff!!!!!"},
+            {usuario: 2, comentario: "Idoloooo"},
+            {usuario: 3, comentario: "Vamos seleccioooonnnn"},
+            {usuario: 5, comentario: "Vinotiintooooo"}
+        ]
+    }
+    ,
     {
         id: 8,
         usuarioId: 4,
-        url: "https://i.pinimg.com/736x/49/19/a8/4919a8dc44184ea27159ad8e1e6dde41.jpg" ,
-        pieImg: "Parado no bailão",
-        comentarios: [{usuario: 2,comentario:"Vamos Santos !!!!!"},{usuario: 3,comentario:"Te amo Neymar !!!!!"},{usuario: 4,comentario:"La copa libertadores es mi obsesion"},{usuario: 5,comentario:"Que Jogadorrrr..."}]
-    },
+        url: "https://i.pinimg.com/originals/6e/9a/61/6e9a612e1f3ea6344bc952e570e79ad8.jpg",
+        pieImg: "Baila Dibu Baila",
+        comentarios: [
+            {usuario: 1, comentario: "SIIIIII"},
+            {usuario: 2, comentario: "Dibujeeeeee"},
+            {usuario: 3, comentario: "LO SIENTO PERO TE COMO HERMANO"},
+            {usuario: 5, comentario: "Genioooooo"}
+        ]
+    }
+    ,
     {
         id: 9,
         usuarioId: 5,
-        url: "https://i.pinimg.com/736x/49/19/a8/4919a8dc44184ea27159ad8e1e6dde41.jpg" ,
-        pieImg: "Parado no bailão",
-        comentarios: [{usuario: 2,comentario:"Vamos Santos !!!!!"},{usuario: 3,comentario:"Te amo Neymar !!!!!"},{usuario: 4,comentario:"La copa libertadores es mi obsesion"},{usuario: 5,comentario:"Que Jogadorrrr..."}]
-    },
+        url: "https://i.pinimg.com/originals/e2/c1/bc/e2c1bcfe56dbf10c545d06cf2c00512f.jpg",
+        pieImg: "+3, Vamos globo!",
+        comentarios: [
+            {usuario: 1, comentario: "VAMO ARRIBAAA"},
+            {usuario: 2, comentario: "Vamos Globo de mi vidaaaaa"},
+            {usuario: 3, comentario: "Como te quiero bigoteeeeeee"},
+            {usuario: 4, comentario: "CRACK"}
+        ]
+    }
+    ,
     {
         id: 10,
         usuarioId: 5,
-        url: "https://i.pinimg.com/736x/49/19/a8/4919a8dc44184ea27159ad8e1e6dde41.jpg" ,
-        pieImg: "Parado no bailão",
-        comentarios: [{usuario: 2,comentario:"Vamos Santos !!!!!"},{usuario: 3,comentario:"Te amo Neymar !!!!!"},{usuario: 4,comentario:"La copa libertadores es mi obsesion"},{usuario: 5,comentario:"Que Jogadorrrr..."}]
-    },]
+        url: "https://i.pinimg.com/originals/98/59/a1/9859a1196280b00ef8cb5b1ae49b6268.jpg",
+        pieImg: "Messi debuts for Inter Miami",
+        comentarios: [
+            {usuario: 1, comentario: "Vamos LEOOOOO"},
+            {usuario: 2, comentario: "El mejor de todos los tiempos"},
+            {usuario: 3, comentario: "COME ON MESSIIIII"},
+            {usuario: 4, comentario: "The Last Dance"}
+        ]
+    }
+]
 };
