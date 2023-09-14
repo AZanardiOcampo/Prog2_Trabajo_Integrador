fd9bb37782f2b01934c30eba23ce7cfe614475d3
AZanardiOcampo <azanardiocampo@udesa.edu.ar>
2023-09-14 16:07:44 -0300

    Fotos de perfil y detalle post

--- Proyecto/controllers/posteosController.js
+++ Proyecto/controllers/posteosController.js
@@ -5,14 +5,16 @@ agregarPost: function(req,res){
 
 },
 detallePost: function(req,res){
-    let id = req.params.id
-    let posteoEncontrado = []
-        for (let i = 0; i < data.posteos.length; i++) {
-            if(data.posteos[i].id == id){
-        posteoEncontrado.push(data.posteos[i])
+    let id = req.params.id;
+    let posteoEncontrado = null;
+        
+    for (let i = 0; i < data.posteos.length; i++) {
+        if(data.posteos[i].id == id){
+        posteoEncontrado = data.posteos[i]
              }
         }
-   return res.render('detallePost',{posteo: posteoEncontrado})
+
+    res.render('detallePost',{posteo: posteoEncontrado})
 },
 searchResults: function(req,res){
 
