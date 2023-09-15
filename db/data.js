084294e391ed9e4c4191963bd1b465a85adaa655
AZanardiOcampo <azanardiocampo@udesa.edu.ar>
2023-09-15 00:40:06 -0300

    detalles fotos de perfil y comentarios

--- db/data.js
+++ db/data.js
@@ -2,7 +2,7 @@ const data = {
     usuarios: [{
         nombreUsuario: "goleroexperto" ,
         email: "goleroexperto@gmail.com",
-        fotoPerfil: null,
+        fotoPerfil: "https://i.pinimg.com/originals/24/1b/8a/241b8a8ac4d409581bf664f3312f64f5.jpg",
         clave: "golero123" ,
         fecNac: "1998-09-24",
         dni: 98765432
@@ -10,7 +10,7 @@ const data = {
     {
         nombreUsuario: "futbolamante",
         email: "futbolamante@gmail.com",
-        fotoPerfil: null,
+        fotoPerfil: "https://i.pinimg.com/originals/13/56/5b/13565bb630a44bf1bee6fdfad7af309f.jpg",
         clave: "amantefulbo",
         fecNac: "2004-03-05",
         dni: 36765432
@@ -18,7 +18,7 @@ const data = {
     {
         nombreUsuario: "jugadorestrella",
         email: "jugadorestrella@gmail.com",
-        fotoPerfil: null,
+        fotoPerfil: "https://i.pinimg.com/originals/c8/bd/78/c8bd78ec453b025407c2ce02bf2ba55f.jpg",
         clave: "star",
         fecNac: "1987-07-31",
         dni: 47955432
@@ -26,7 +26,7 @@ const data = {
     {
         nombreUsuario: "campodejuego",
         email: "campodejuego@gmail.com",
-        fotoPerfil: null,
+        fotoPerfil: "https://i.pinimg.com/originals/df/4f/9f/df4f9f97d31c38cdd05bdbc6f5c6df0b.jpg",
         clave: "campo",
         fecNac: "2012-01-06",
         dni: 47928032
@@ -34,7 +34,7 @@ const data = {
     {
         nombreUsuario: "entrenadorhabil",
         email: "entrenadorhabil@gmail.com",
-        fotoPerfil: null,
+        fotoPerfil: "https://i.pinimg.com/originals/76/3c/cc/763ccc261482595ed42c94f76ddd6764.jpg",
         clave: "dt",
         fecNac: "1987-06-24",
         dni: 55674032
@@ -44,135 +44,140 @@ const data = {
         id: 1,
         usuarioId: 1,
         nombreUsuario: "goleroexperto",
-        url: "https://i.pinimg.com/736x/49/19/a8/4919a8dc44184ea27159ad8e1e6dde41.jpg" ,
+        fotoPerfil: "https://i.pinimg.com/originals/24/1b/8a/241b8a8ac4d409581bf664f3312f64f5.jpg",
+        url: "https://i.pinimg.com/736x/49/19/a8/4919a8dc44184ea27159ad8e1e6dde41.jpg",
         pieImg: "Parado no bailão",
         comentarios: [
-            {usuario: "futbolamante",comentario:"Vamos Santos !!!!!"},
-            {usuario: "jugadorestrella",comentario:"Te amo Neymar !!!!!"},
-            {usuario: "campodejuego",comentario:"La copa libertadores es mi obsesion"},
-            {usuario: "entrenadorhabil",comentario:"Que Jogadorrrr..."}]
+            { usuario: "futbolamante", usuarioId: 2, comentario: "Vamos Santos !!!!!", fotoPerfil: "https://i.pinimg.com/originals/13/56/5b/13565bb630a44bf1bee6fdfad7af309f.jpg" },
+            { usuario: "jugadorestrella", usuarioId: 3, comentario: "Te amo Neymar !!!!!", fotoPerfil: "https://i.pinimg.com/originals/c8/bd/78/c8bd78ec453b025407c2ce02bf2ba55f.jpg" },
+            { usuario: "campodejuego", usuarioId: 4, comentario: "La copa libertadores es mi obsesion", fotoPerfil: "https://i.pinimg.com/originals/df/4f/9f/df4f9f97d31c38cdd05bdbc6f5c6df0b.jpg" },
+            { usuario: "entrenadorhabil", usuarioId: 5, comentario: "Que Jogadorrrr...", fotoPerfil: "https://i.pinimg.com/originals/76/3c/cc/763ccc261482595ed42c94f76ddd6764.jpg" }
+        ]
+    },
+    {
+        id: 4,
+        usuarioId: 2,
+        nombreUsuario: "futbolamante",
+        fotoPerfil: "https://i.pinimg.com/originals/13/56/5b/13565bb630a44bf1bee6fdfad7af309f.jpg",
+        url: "https://i.pinimg.com/originals/34/21/2e/34212e4bf0581d9f528907987ff40f58.jpg",
+        pieImg: "Caipirinha, samba e futebol",
+        comentarios: [
+            { usuario: "goleroexperto", usuarioId: 1, comentario: "Zaaambaaaaa", fotoPerfil: "https://i.pinimg.com/originals/24/1b/8a/241b8a8ac4d409581bf664f3312f64f5.jpg" },
+            { usuario: "jugadorestrella", usuarioId: 3, comentario: "The magician", fotoPerfil: "https://i.pinimg.com/originals/c8/bd/78/c8bd78ec453b025407c2ce02bf2ba55f.jpg" },
+            { usuario: "campodejuego", usuarioId: 4, comentario: "Futeboolll", fotoPerfil: "https://i.pinimg.com/originals/df/4f/9f/df4f9f97d31c38cdd05bdbc6f5c6df0b.jpg" },
+            { usuario: "entrenadorhabil", usuarioId: 5, comentario: "Baila Dinho Baila", fotoPerfil: "https://i.pinimg.com/originals/76/3c/cc/763ccc261482595ed42c94f76ddd6764.jpg" }
+        ]
+    },
+    {
+        id: 5,
+        usuarioId: 3,
+        nombreUsuario: "jugadorestrella",
+        fotoPerfil: "https://i.pinimg.com/originals/c8/bd/78/c8bd78ec453b025407c2ce02bf2ba55f.jpg",
+        url: "https://i.pinimg.com/originals/e5/33/20/e533200b8ffded0453a22c0cde450585.jpg",
+        pieImg: "TE AMO ARGENTINAAAA <3 <3",
+        comentarios: [
+            { usuario: "goleroexperto", usuarioId: 1, comentario: "THE GOAT", fotoPerfil: "https://i.pinimg.com/originals/24/1b/8a/241b8a8ac4d409581bf664f3312f64f5.jpg" },
+            { usuario: "futbolamante", usuarioId: 2, comentario: "Muchaaaachooosssss", fotoPerfil: "https://i.pinimg.com/originals/13/56/5b/13565bb630a44bf1bee6fdfad7af309f.jpg" },
+            { usuario: "campodejuego", usuarioId: 4, comentario: "Ronaldo > Messi", fotoPerfil: "https://i.pinimg.com/originals/df/4f/9f/df4f9f97d31c38cdd05bdbc6f5c6df0b.jpg" },
+            { usuario: "entrenadorhabil", usuarioId: 5, comentario: "HERMOSAAAAA", fotoPerfil: "https://i.pinimg.com/originals/76/3c/cc/763ccc261482595ed42c94f76ddd6764.jpg" }
+        ]
+    },
+    {
+        id: 7,
+        usuarioId: 4,
+        nombreUsuario: "campodejuego",
+        fotoPerfil: "https://i.pinimg.com/originals/df/4f/9f/df4f9f97d31c38cdd05bdbc6f5c6df0b.jpg",
+        url: "https://i.pinimg.com/originals/ad/f1/a6/adf1a69d625058b82066e88c76665b41.jpg",
+        pieImg: "Vinotinto de mi vida...",
+        comentarios: [
+            { usuario: "goleroexperto", usuarioId: 1, comentario: "Grande Josefff!!!!!", fotoPerfil: "https://i.pinimg.com/originals/24/1b/8a/241b8a8ac4d409581bf664f3312f64f5.jpg" },
+            { usuario: "futbolamante", usuarioId: 2, comentario: "Idoloooo", fotoPerfil: "https://i.pinimg.com/originals/13/56/5b/13565bb630a44bf1bee6fdfad7af309f.jpg" },
+            { usuario: "jugadorestrella", usuarioId: 3, comentario: "Vamos seleccioooonnnn", fotoPerfil: "https://i.pinimg.com/originals/c8/bd/78/c8bd78ec453b025407c2ce02bf2ba55f.jpg" },
+            { usuario: "entrenadorhabil", usuarioId: 5, comentario: "Vinotiintooooo", fotoPerfil: "https://i.pinimg.com/originals/76/3c/cc/763ccc261482595ed42c94f76ddd6764.jpg" }
+        ]
+    },
+    {
+        id: 9,
+        usuarioId: 5,
+        nombreUsuario: "entrenadorhabil",
+        fotoPerfil: "https://i.pinimg.com/originals/76/3c/cc/763ccc261482595ed42c94f76ddd6764.jpg",
+        url: "https://i.pinimg.com/originals/e2/c1/bc/e2c1bcfe56dbf10c545d06cf2c00512f.jpg",
+        pieImg: "+3, Vamos globo!",
+        comentarios: [
+            { usuario: "goleroexperto", usuarioId: 1, comentario: "VAMO ARRIBAAA", fotoPerfil: "https://i.pinimg.com/originals/24/1b/8a/241b8a8ac4d409581bf664f3312f64f5.jpg" },
+            { usuario: "futbolamante", usuarioId: 2, comentario: "Vamos Globo de mi vidaaaaa", fotoPerfil: "https://i.pinimg.com/originals/13/56/5b/13565bb630a44bf1bee6fdfad7af309f.jpg" },
+            { usuario: "jugadorestrella", usuarioId: 3, comentario: "Como te quiero bigoteeeeeee", fotoPerfil: "https://i.pinimg.com/originals/c8/bd/78/c8bd78ec453b025407c2ce02bf2ba55f.jpg" },
+            { usuario: "campodejuego", usuarioId: 4, comentario: "CRACK", fotoPerfil: "https://i.pinimg.com/originals/df/4f/9f/df4f9f97d31c38cdd05bdbc6f5c6df0b.jpg" }
+        ]
     },
     {
         id: 2,
         usuarioId: 1,
         nombreUsuario: "goleroexperto",
-        url: "https://i.pinimg.com/originals/30/56/6b/30566b73b45265855eec978630163a9e.jpg" ,
+        fotoPerfil: "https://i.pinimg.com/originals/24/1b/8a/241b8a8ac4d409581bf664f3312f64f5.jpg",
+        url: "https://i.pinimg.com/originals/30/56/6b/30566b73b45265855eec978630163a9e.jpg",
         pieImg: "El ultimo Diez",
         comentarios: [
-            {usuario: "futbolamante",comentario:"EL ULTIMO 10"},
-            {usuario: "jugadorestrella",comentario:"Roman, Boca es tu casa"},
-            {usuario: "campodejuego",comentario:"QUIERO LA LIBERTADOOOREEEEE"},
-            {usuario: "entrenadorhabil",comentario:"Romanticoooo"}]
+            { usuario: "futbolamante", usuarioId: 2, comentario: "EL ULTIMO 10", fotoPerfil: "https://i.pinimg.com/originals/13/56/5b/13565bb630a44bf1bee6fdfad7af309f.jpg" },
+            { usuario: "jugadorestrella", usuarioId: 3, comentario: "Roman, Boca es tu casa", fotoPerfil: "https://i.pinimg.com/originals/c8/bd/78/c8bd78ec453b025407c2ce02bf2ba55f.jpg" },
+            { usuario: "campodejuego", usuarioId: 4, comentario: "QUIERO LA LIBERTADOOOREEEEE", fotoPerfil: "https://i.pinimg.com/originals/df/4f/9f/df4f9f97d31c38cdd05bdbc6f5c6df0b.jpg" },
+            { usuario: "entrenadorhabil", usuarioId: 5, comentario: "Romanticoooo", fotoPerfil: "https://i.pinimg.com/originals/76/3c/cc/763ccc261482595ed42c94f76ddd6764.jpg" }
+        ]
     },
     {
         id: 3,
         usuarioId: 2,
         nombreUsuario: "futbolamante",
+        fotoPerfil: "https://i.pinimg.com/originals/13/56/5b/13565bb630a44bf1bee6fdfad7af309f.jpg",
         url: "https://i.pinimg.com/originals/22/ad/21/22ad219ca0f55b0b46c8d79a556a56ae.jpg",
         pieImg: "SIUUUUUUUUUUU",
         comentarios: [
-            {usuario: "goleroexperto", comentario: "THE GOAT"},
-            {usuario: "jugadorestrella", comentario: "Mr. Championsss"},
-            {usuario: "campodejuego", comentario: "EL COMANDANTEEEE"},
-            {usuario: "entrenadorhabil", comentario: "Messi > Ronaldo"}
+            { usuario: "goleroexperto", usuarioId: 1, comentario: "THE GOAT", fotoPerfil: "https://i.pinimg.com/originals/24/1b/8a/241b8a8ac4d409581bf664f3312f64f5.jpg" },
+            { usuario: "jugadorestrella", usuarioId: 3, comentario: "Mr. Championsss", fotoPerfil: "https://i.pinimg.com/originals/c8/bd/78/c8bd78ec453b025407c2ce02bf2ba55f.jpg" },
+            { usuario: "campodejuego", usuarioId: 4, comentario: "EL COMANDANTEEEE", fotoPerfil: "https://i.pinimg.com/originals/df/4f/9f/df4f9f97d31c38cdd05bdbc6f5c6df0b.jpg" },
+            { usuario: "entrenadorhabil", usuarioId: 5, comentario: "Messi > Ronaldo", fotoPerfil: "https://i.pinimg.com/originals/76/3c/cc/763ccc261482595ed42c94f76ddd6764.jpg" }
         ]
-    }
-    ,
-    {
-        id: 4,
-        usuarioId: 2,
-        nombreUsuario: "futbolamante",
-        url: "https://i.pinimg.com/originals/34/21/2e/34212e4bf0581d9f528907987ff40f58.jpg",
-        pieImg: "Caipirinha, samba e futebol",
-        comentarios: [
-            {usuario: "goleroexperto", comentario: "Zaaambaaaaa"},
-            {usuario: "jugadorestrella", comentario: "The magician"},
-            {usuario: "campodejuego", comentario: "Futeboolll"},
-            {usuario: "entrenadorhabil", comentario: "Baila Dinho Baila"}
-        ]
-    }
-    ,
-    {
-        id: 5,
-        usuarioId: 3,
-        nombreUsuario: "jugadorestrella",
-        url: "https://i.pinimg.com/originals/e5/33/20/e533200b8ffded0453a22c0cde450585.jpg",
-        pieImg: "TE AMO ARGENTINAAAA <3 <3",
-        comentarios: [
-            {usuario: "goleroexperto", comentario: "THE GOAT"},
-            {usuario: "futbolamante", comentario: "Muchaaaachooosssss"},
-            {usuario: "campodejuego", comentario: "Ronaldo > Messi"},
-            {usuario: "entrenadorhabil", comentario: "HERMOSAAAAA"}
-        ]
-    }
-    ,
+    },
     {
         id: 6,
         usuarioId: 3,
         nombreUsuario: "jugadorestrella",
+        fotoPerfil: "https://i.pinimg.com/originals/c8/bd/78/c8bd78ec453b025407c2ce02bf2ba55f.jpg",
         url: "https://i.pinimg.com/originals/f4/b3/6c/f4b36cef73d4dbbaf1a765cc97ed2b00.jpg",
         pieImg: "Kikando e me olhandooo",
         comentarios: [
-            {usuario: "goleroexperto", comentario: "Visca Barca"},
-            {usuario: "futbolamante", comentario: "Volveeeeee"},
-            {usuario: "campodejuego", comentario: "Neymar Barcelona > Neymar PSG"},
-            {usuario: "entrenadorhabil", comentario: "Campeonesssss"}
+            { usuario: "goleroexperto", usuarioId: 1, comentario: "Visca Barca", fotoPerfil: "https://i.pinimg.com/originals/24/1b/8a/241b8a8ac4d409581bf664f3312f64f5.jpg" },
+            { usuario: "futbolamante", usuarioId: 2, comentario: "Volveeeeee", fotoPerfil: "https://i.pinimg.com/originals/13/56/5b/13565bb630a44bf1bee6fdfad7af309f.jpg" },
+            { usuario: "campodejuego", usuarioId: 4, comentario: "Neymar Barcelona > Neymar PSG", fotoPerfil: "https://i.pinimg.com/originals/df/4f/9f/df4f9f97d31c38cdd05bdbc6f5c6df0b.jpg" },
+            { usuario: "entrenadorhabil", usuarioId: 5, comentario: "Campeonesssss", fotoPerfil: "https://i.pinimg.com/originals/76/3c/cc/763ccc261482595ed42c94f76ddd6764.jpg" }
         ]
-    }
-    ,
-    {
-        id: 7,
-        usuarioId: 4,
-        nombreUsuario: "campodejuego",
-        url: "https://i.pinimg.com/originals/ad/f1/a6/adf1a69d625058b82066e88c76665b41.jpg",
-        pieImg: "Vinotinto de mi vida...",
-        comentarios: [
-            {usuario: "goleroexperto", comentario: "Grande Josefff!!!!!"},
-            {usuario: "futbolamante", comentario: "Idoloooo"},
-            {usuario: "jugadorestrella", comentario: "Vamos seleccioooonnnn"},
-            {usuario: "entrenadorhabil", comentario: "Vinotiintooooo"}
-        ]
-    }
-    ,
+    },
     {
         id: 8,
         usuarioId: 4,
         nombreUsuario: "campodejuego",
+        fotoPerfil: "https://i.pinimg.com/originals/df/4f/9f/df4f9f97d31c38cdd05bdbc6f5c6df0b.jpg",
         url: "https://i.pinimg.com/originals/6e/9a/61/6e9a612e1f3ea6344bc952e570e79ad8.jpg",
         pieImg: "Baila Dibu Baila",
         comentarios: [
-            {usuario: "goleroexperto", comentario: "SIIIIII"},
-            {usuario: "futbolamante", comentario: "Dibujeeeeee"},
-            {usuario: "jugadorestrella", comentario: "LO SIENTO PERO TE COMO HERMANO"},
-            {usuario: "entrenadorhabil", comentario: "Genioooooo"}
+            { usuario: "goleroexperto", usuarioId: 1, comentario: "SIIIIII", fotoPerfil: "https://i.pinimg.com/originals/24/1b/8a/241b8a8ac4d409581bf664f3312f64f5.jpg" },
+            { usuario: "futbolamante", usuarioId: 2, comentario: "Dibujeeeeee", fotoPerfil: "https://i.pinimg.com/originals/13/56/5b/13565bb630a44bf1bee6fdfad7af309f.jpg" },
+            { usuario: "jugadorestrella", usuarioId: 3, comentario: "LO SIENTO PERO TE COMO HERMANO", fotoPerfil: "https://i.pinimg.com/originals/c8/bd/78/c8bd78ec453b025407c2ce02bf2ba55f.jpg" },
+            { usuario: "entrenadorhabil", usuarioId: 5, comentario: "Genioooooo", fotoPerfil: "https://i.pinimg.com/originals/76/3c/cc/763ccc261482595ed42c94f76ddd6764.jpg" }
         ]
-    }
-    ,
-    {
-        id: 9,
-        usuarioId: 5,
-        nombreUsuario: "entrenadorhabil",
-        url: "https://i.pinimg.com/originals/e2/c1/bc/e2c1bcfe56dbf10c545d06cf2c00512f.jpg",
-        pieImg: "+3, Vamos globo!",
-        comentarios: [
-            {usuario: "goleroexperto", comentario: "VAMO ARRIBAAA"},
-            {usuario: "futbolamante", comentario: "Vamos Globo de mi vidaaaaa"},
-            {usuario: "jugadorestrella", comentario: "Como te quiero bigoteeeeeee"},
-            {usuario: "campodejuego", comentario: "CRACK"}
-        ]
-    }
-    ,
+    },
     {
         id: 10,
         usuarioId: 5,
         nombreUsuario: "entrenadorhabil",
+        fotoPerfil: "https://i.pinimg.com/originals/76/3c/cc/763ccc261482595ed42c94f76ddd6764.jpg",
         url: "https://i.pinimg.com/originals/98/59/a1/9859a1196280b00ef8cb5b1ae49b6268.jpg",
         pieImg: "Messi debuts for Inter Miami",
         comentarios: [
-            {usuario: "goleroexperto", comentario: "Vamos LEOOOOO"},
-            {usuario: "futbolamante", comentario: "El mejor de todos los tiempos"},
-            {usuario: "jugadorestrella", comentario: "COME ON MESSIIIII"},
-            {usuario: "campodejuego", comentario: "The Last Dance"}
+            { usuario: "goleroexperto", usuarioId: 1, comentario: "Vamos LEOOOOO", fotoPerfil: "https://i.pinimg.com/originals/24/1b/8a/241b8a8ac4d409581bf664f3312f64f5.jpg" },
+            { usuario: "futbolamante", usuarioId: 2, comentario: "El mejor de todos los tiempos", fotoPerfil: "https://i.pinimg.com/originals/13/56/5b/13565bb630a44bf1bee6fdfad7af309f.jpg" },
+            { usuario: "jugadorestrella", usuarioId: 3, comentario: "COME ON MESSIIIII", fotoPerfil: "https://i.pinimg.com/originals/c8/bd/78/c8bd78ec453b025407c2ce02bf2ba55f.jpg" },
+            { usuario: "campodejuego", usuarioId: 4, comentario: "The Last Dance", fotoPerfil: "https://i.pinimg.com/originals/df/4f/9f/df4f9f97d31c38cdd05bdbc6f5c6df0b.jpg" }
         ]
     }
 ]
